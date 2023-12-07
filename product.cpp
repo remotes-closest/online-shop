11122c8819397ff479e59a49781e5aecf2d48d8b
Emma Johnson <153087246+remotes-closest@users.noreply.github.com>
2023-12-07 13:38:51 +0700

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,15 @@
+#include "product.h"
+
+Product::Product(int id, const std::string& name, double price) : id(id), name(name), price(price) {}
+
+int Product::getId() const {
+    return id;
+}
+
+const std::string& Product::getName() const {
+    return name;
+}
+
+double Product::getPrice() const {
+    return price;
+}
