a2921a904b06482358f2457a5feadfcafe474b28
Emma Johnson <153087246+remotes-closest@users.noreply.github.com>
2023-12-07 13:38:39 +0700

    Create product.h

---
+++ product.h
@@ -0,0 +1,20 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include <string>
+
+class Product {
+public:
+    Product(int id, const std::string& name, double price);
+
+    int getId() const;
+    const std::string& getName() const;
+    double getPrice() const;
+
+private:
+    int id;
+    std::string name;
+    double price;
+};
+
+#endif
